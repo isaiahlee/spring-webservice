afa5d9ccb628a580a5c4b4cd5f4e57999fc3594b
isaiahlee224 <isaiahlee224@gmail.com>
2019-01-11 17:54:32 +0900

    nginx 무중단 배포 구축하기

--- src/main/java/com/isaiahlee224/webservice/web/WebRestController.java
+++ src/main/java/com/isaiahlee224/webservice/web/WebRestController.java
@@ -1,21 +1,23 @@
 package com.isaiahlee224.webservice.web;
 
 import com.isaiahlee224.webservice.domain.common.CommonResponse;
-import com.isaiahlee224.webservice.domain.posts.PostsRepository;
 import com.isaiahlee224.webservice.dto.PostsSaveRequestDto;
 import com.isaiahlee224.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Arrays;
+
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
-    private PostsRepository postsRepository;
     private PostsService postsService;
+    private Environment env;
 
     @GetMapping("/hello")
     public String hello() {
@@ -30,5 +32,12 @@ public class WebRestController {
                 .result("OK")
                 .message("msg")
                 .build();
+    }
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
     }
 }
