640d69bd6fc33e6746dc41dc401bb6c94327f24a
이이사야 <isaiahlee224@boneis.com>
2019-01-18 17:12:12 +0900

    real환경

--- src/main/java/com/isaiahlee224/webservice/Application.java
+++ src/main/java/com/isaiahlee224/webservice/Application.java
@@ -10,6 +10,7 @@ public class Application {
 
     public static final String APPLICATION_LOCATIONS = "spring.config.location="
             + "classpath:application.yml,"
+            + "e:/DEV/test_config/real-application.yml,"
             + "/app/config/spring-webservice/real-application.yml";
 
     public static void main(String[] args) {
--- src/test/java/com/isaiahlee224/webservice/web/WebControllerTest.java
+++ src/test/java/com/isaiahlee224/webservice/web/WebControllerTest.java
@@ -12,17 +12,17 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = RANDOM_PORT)
-public class WebRestControllerTest {
+public class WebControllerTest {
 
     @Autowired
     private TestRestTemplate restTemplate;
 
     @Test
-    public void Profile확인() {
+    public void 메인페이지_로딩() {
         //when
-        String profile = this.restTemplate.getForObject("/profile", String.class);
+        String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(profile).isEqualTo("local");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 }
